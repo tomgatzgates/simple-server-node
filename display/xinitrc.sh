97e642a5dbb7db42a98873f84c5d7f4dd6ae762f
Tom <tom@pactcoffee.com>
2017-03-01 21:58:30 +0000

    fix up

--- display/xinitrc.sh
+++ display/xinitrc.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
 while true; do
 
-    # Clean up previously running apps, gracefully at first then harshly
-    killall -TERM chromium-browser 2>/dev/null;
-    killall -TERM matchbox-window-manager 2>/dev/null;
-    sleep 2;
-    killall -9 chromium-browser 2>/dev/null;
-    killall -9 matchbox-window-manager 2>/dev/null;
+	# Clean up previously running apps, gracefully at first then harshly
+	killall -TERM epiphany 2>/dev/null;
+	killall -TERM matchbox-window-manager 2>/dev/null;
+	sleep 2;
+	killall -9 epiphany 2>/dev/null;
+	killall -9 matchbox-window-manager 2>/dev/null;
 
-    # Clean out existing profile information
-    rm -rf /home/pi/.cache;
-    rm -rf /home/pi/.config;
-    rm -rf /home/pi/.pki;
+	# Clean out existing profile information
+	rm -rf /root/.cache;
+	rm -rf /root/.config;
+	rm -rf /root/.pki;
 
-    # Generate the bare minimum to keep Chromium happy!
-    mkdir -p /home/pi/.config/chromium/Default
-    sqlite3 /home/pi/.config/chromium/Default/Web\ Data "CREATE TABLE meta(key LONGVARCHAR NOT NULL UNIQUE PRIMARY KEY, value LONGVARCHAR); INSERT INTO meta VALUES('version','46'); CREATE TABLE keywords (foo INTEGER);";
+	# Generate the bare minimum to keep Chromium happy!
+	# mkdir -p /root/.config/epiphany/Default
+	# sqlite3 /root/.config/epiphany/Default/Web\ Data "CREATE TABLE meta(key LONGVARCHAR NOT NULL UNIQUE PRIMARY KEY, value LONGVARCHAR); INSERT INTO meta VALUES('version','46'); CREATE TABLE keywords (foo INTEGER);";
 
-    # Generate the bare minimum to keep Epiphany happy!
-    mkdir -p /home/pi/.config/epiphany/Default
+	# Disable DPMS / Screen blanking
+	xset -dpms
+	xset s off
 
-    # Disable DPMS / Screen blanking
-    xset -dpms
-    xset s off
+	# Reset the framebuffer's colour-depth
+	fbset -depth $( cat /sys/module/*fb*/parameters/fbdepth );
 
-    # Reset the framebuffer's colour-depth
-    fbset -depth $( cat /sys/module/*fb*/parameters/fbdepth );
+	# Hide the cursor (move it to the bottom-right, comment out if you want mouse interaction)
+	xwit -root -warp $( cat /sys/module/*fb*/parameters/fbwidth ) $( cat /sys/module/*fb*/parameters/fbheight )
 
-    # Hide the cursor (move it to the bottom-right, comment out if you want mouse interaction)
-    xwit -root -warp $( cat /sys/module/*fb*/parameters/fbwidth ) $( cat /sys/module/*fb*/parameters/fbheight )
+	# Start the window manager (remove "-use_cursor no" if you actually want mouse interaction)
+	matchbox-window-manager -use_titlebar no -use_cursor no &
 
-    # Start the window manager (remove "-use_cursor no" if you actually want mouse interaction)
-    matchbox-window-manager -use_titlebar no -use_cursor no &
+    # chrome data directory
+    rm -rf /data/chrome
+    [ ! -d /data/chrome ] && mkdir -p /data/chrome
 
-    # Start the browser (See http://peter.sh/experiments/chromium-command-line-switches/)
-    # chromium-browser  --app='https://www.bbc.co.uk'
-    epiphany-browser -a --profile /home/pi/.config/epiphany/Default  https://www.bbc.co.uk
+  mkdir ~/.config
+	epiphany-browser -a --profile ~/.config https://www.bbc.co.uk
 
 done;
